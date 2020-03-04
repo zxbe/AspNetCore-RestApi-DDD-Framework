ee9ef3064a97929c055749d92b126dba7ec4fff5
Дмитрий Фоменко <dfomenko@mmtr.ru>
2020-03-04 16:28:05 +0300

    upd

---
+++ Domain/Base/ResultContainer.cs
@@ -0,0 +1,22 @@
+﻿﻿using Domain.Error;
+
+namespace Domain.Base
+{
+    public class ResultContainer<T>
+    {
+        public ResultContainer(T result)
+        {
+            Result = result;
+        }
+
+        public ResultContainer(ErrorCodes errorCode, string errorField)
+        {
+            Error = errorCode;
+            ErrorField = errorField;
+        }
+
+        public ErrorCodes? Error { get; set; }
+        public string ErrorField { get; set; }
+        public T Result { get; }
+    }
+}
